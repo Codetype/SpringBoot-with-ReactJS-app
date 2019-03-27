3da245af343afd756a03f5f87a301c068fd9670a
Paweł Gędłek <pawelgedlek@gmail.com>
2019-03-27 20:45:47 +0100

    Commit REST methods to app

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -8,13 +8,87 @@ class App extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {employees: []};
+		this.state = {employees: [], attributes: [], pageSize: 2, links: {}};
+		this.updatePageSize = this.updatePageSize.bind(this);
+		this.onCreate = this.onCreate.bind(this);
+		this.onDelete = this.onDelete.bind(this);
+		this.onNavigate = this.onNavigate.bind(this);
+	}
+
+	loadFromServer(pageSize) {
+		follow(client, root, [
+			{rel: 'employees', params: {size: pageSize}}
+		]).then(employeeCollection => {
+			return client({
+				method: 'GET',
+				path: employeeCollection.entity._links.profile.href,
+				headers: {'Accept': 'application/schema+json'}
+			}).then(schema => {
+				this.schema = schema.entity;
+				return employeeCollection;
+			});
+		}).done(employeeCollection => {
+			this.setState({
+				employees: employeeCollection.entity._embedded.employees,
+				attributes: Object.keys(this.schema.properties),
+				pageSize: pageSize,
+				links: employeeCollection.entity._links
+			})
+		});
+	}
+
+	onCreate(newEmployee) {
+		follow(client, root, ['employees']).then(employeeCollection => {
+			return client({
+				method: 'POST',
+				path: employeeCollection.entity._links.self.href,
+				entity: newEmployee,
+				headers: {'Accept': 'application/json'}
+				})
+		}).then(response => {
+				return follow(client, root, [
+					{rel: 'employees', params: {'size': this.state.pageSize}}
+				]);
+		}).done(response => {
+			if(typeof response.entity._links.last !== "undefined") {
+				this.onNavigate(response.entity._links.last.href);
+			} else {
+				this.onNavigate(response.entity._links.self.href);
+			}
+		});
+	}
+
+	onDelete(employee) {
+		client({
+			method: 'DELETE',
+			path: employee._links.self.href,
+			}).done(response => {
+				this.loadFromServer(this.state.pageSize);
+			});
+	}
+
+	onNavigate(navUri) {
+		client({
+			method: 'GET',
+			path: navUri,
+		}).done(employeeCollection => {
+			this.setState({
+				employees: employeeCollection.entity._embedded.employees,
+				attributes: this.state.attributes,
+				pageSize: this.state.pageSize,
+				links: employeeCollection.entity._links
+			});
+		});
+	}
+
+	updatePageSize(pageSize) {
+		if (pageSize !== this.state.pageSize){
+			this.loadFromServer(pageSize);
+		}
 	}
 
 	componentDidMount() {
-		client({method: 'GET', path: '/api/employees'}).done(response => {
-			this.setState({employees: response.entity._embedded.employees});
-		});
+		this.loadFromServer(this.state.pageSize);
     }
 
 	render() {
